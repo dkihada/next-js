091d909e1f1b503403bb7477edf39dc08d64d3ec
Denis Agureev <dkihada@gmail.com>
2021-10-09 14:22:57 +0500

    create getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,38 @@
-export default function Home(): JSX.Element {
-	return <></>;
+import { GetStaticProps } from 'next';
+import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
+
+function Home({ menu }: HomeProps): JSX.Element {
+	return (
+		<>
+			<ul>
+				{menu.map((m) => (
+					<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+				))}
+			</ul>
+		</>
+	);
+}
+
+export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(
+		process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+		{ firstCategory }
+	);
+
+	return {
+		props: {
+			menu,
+			firstCategory,
+		},
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
 }
