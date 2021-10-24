b2992d87aa66f36f3f96b9a6511134ba07793897
dkihada <dkihada@gmail.com>
2021-10-25 01:08:00 +0500

    create declOfNum helper

--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -40,3 +40,15 @@ export const priceRu = (price: number): string =>
 
 export const numSpace = (num: number): string =>
 	num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+
+export const declOfNum = (
+	number: number,
+	titles: [string, string, string]
+): string => {
+	const cases = [2, 0, 1, 1, 1, 2];
+	return titles[
+		number % 100 > 4 && number % 100 < 20
+			? 2
+			: cases[number % 10 < 5 ? number % 10 : 5]
+	];
+};
